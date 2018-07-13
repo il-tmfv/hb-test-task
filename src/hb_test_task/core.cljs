03b3e2b7cd92b2707a3cbe9ade2d85f78df90de3
Ilya Timofeev <il.tmfv@gmail.com>
2018-07-13 23:31:03 +0300

    Add basic events handling

--- src/hb_test_task/core.cljs
+++ src/hb_test_task/core.cljs
@@ -9,9 +9,11 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (r/atom {:selected-country "1"}))
+(defonce app-state (r/atom {:selected-country "0"
+                            :input-value      ""}))
 
-(defonce selected-country (r/cursor app-state [:selected-country]))
+(def selected-country (r/cursor app-state [:selected-country]))
+(def input-value (r/cursor app-state [:input-value]))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
@@ -19,22 +21,40 @@
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
   )
 
-(defonce country-db [{:id           "1"
-                      :label        "UK"
-                      :flag         "🇬🇧"
-                      :country-code "+44"
-                      :phone-format "+44 (##) #### ####"}
-                     {:id           "2"
-                      :label        "US"
-                      :flag         "🇺🇸"
-                      :country-code "+1"
-                      :phone-format "+1 (###) ### ####"}])
+(defn on-select-change [e]
+  (let [new-country-id (-> e .-target .-value)]
+    (reset! selected-country new-country-id)))
+
+(defn on-input-change [e]
+  (let [new-phone-number (-> e .-target .-value)]
+    (reset! input-value new-phone-number)))
+
+(def country-db [{:id           "0"
+                  :label        "??"
+                  :flag         "🏴"
+                  :country-code "+44"
+                  :phone-format "+44 (##) #### ####"}
+                 {:id           "1"
+                  :label        "UK"
+                  :flag         "🇬🇧"
+                  :country-code "+44"
+                  :phone-format "+44 (##) #### ####"}
+                 {:id           "2"
+                  :label        "US"
+                  :flag         "🇺🇸"
+                  :country-code "+1"
+                  :phone-format "+1 (###) ### ####"}
+                 {:id           "3"
+                  :label        "RU"
+                  :flag         "🇷🇺"
+                  :country-code "+7"
+                  :phone-format "+7 (###) ### ####"}])
 
 (r/render [app
            [phone-input {:options          country-db
-                         :input-value      ""
+                         :input-value      input-value
                          :hint             "hint text"
-                         :on-input-change  #()
-                         :on-select-change #()
-                         :select-value     @selected-country}]]
+                         :on-input-change  on-input-change
+                         :on-select-change on-select-change
+                         :select-value     selected-country}]]
           (.getElementById js/document "app"))
